26879f34a8fb0fe4ba8557a7ec3468a756a274cf
robbyq92 <49034238+robbyq92@users.noreply.github.com>
2020-07-24 16:56:59 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,16 +20,16 @@ variable "amis" {
 
 variable "vpc_cidr" {
   description = "CIDR para toda la VPC"
-  default     = "10.0.0.0/16"
+  default     = "20.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   description = "CIDR para la subred Public"
-  default     = "10.0.0.0/24"
+  default     = "20.0.0.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR para la subred Private"
-  default     = "10.0.1.0/24"
+  default     = "20.0.1.0/24"
 }
 
